3e21abadfeb274557008ca791b8bb192f605a23c
bdi-carlo <benjamin.97@live.fr>
2022-09-16 22:28:10 +0200

    Init

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Alert, FlatList, ImageBackground, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  
+  playPressed = ()=>{
+    Alert.alert("A venir")
+  }
+
+  historyPressed = ()=>{
+    Alert.alert("A venir")
+  }
+  
+  render() {
+    return (
+      <ImageBackground
+        source={require('./assets/wallpaper.png')}
+        style={styles.background}
+      >
+        <View style={styles.container}>
+          <Text style={styles.title}>Golf 3 Points</Text>
+          <StatusBar style="auto" />
+
+          <View style={styles.gridButtons}>
+            <TouchableOpacity style={styles.btn} onPress={this.playPressed}>
+                <Text style={styles.button}>JOUER</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.historyPressed}>
+                <Text style={styles.button}>HISTORIQUE</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.historyPressed}>
+                <Text style={styles.button}>JOUEURS</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.historyPressed}>
+                <Text style={styles.button}>STATS</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
+    );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
+    width: '100%',
+    height: '100%'
+  },
+  title: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginTop: "25%",
+    fontSize:  50,
+  },
+  gridButtons: {
+    flex: 1,
+  },
+  button: {
+    backgroundColor: '#3A59FF',
+    color: 'white',
+    borderRadius: 10,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    margin: "1%",
+    fontSize:  27
+  },
+  container: {
+    flex: 1
   },
 });
