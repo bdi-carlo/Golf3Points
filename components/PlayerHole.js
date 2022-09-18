37c4656a05a1ccd1b526b9cb7d5585fe08ad2581
bdi-carlo <benjamin.97@live.fr>
2022-09-19 00:03:21 +0200

    Add dynamics PlayerHole components

--- components/PlayerHole.js
+++ components/PlayerHole.js
@@ -1,29 +1,123 @@
-import React from "react";
-import { View } from "react-native";
 import CheckBox from "expo-checkbox"
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Button, TouchableOpacity } from "react-native";
 
-export default class PlayerHole extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            score : 0,
-            fairway : false,
-            putter : 0
-        }
+export default function PlayerHole(props){
+    const playerName = props.playerName;
+    const [isFairway, setFairway] = useState(false);
+    const [isRegul, setRegul] = useState(false);
+    const [isCloser, setCloser] = useState(false);
+    var [nbPutts, setPutts] = useState(2);
+    var [nbShots, setShots] = useState(props.nbPar);
+    var [score, setScore] = useState(0);
+
+    useEffect(() => {updateScore();});
+
+    function incrementPutts(){
+        nbPutts = nbPutts + 1;
+        setPutts(nbPutts);
     }
 
-    setFairway(){
-        console.log("test");
-        this.state.fairway = true;
+    function decrementPutts(){
+        nbPutts > 0 ? nbPutts = nbPutts - 1 : null;
+        setPutts(nbPutts);
     }
 
-    render(){
-        return(
-            <View>
-                <CheckBox 
-                    value={this.state.fairway}
-                    onValueChange={this.setFairway(this)}/>
+    function incrementShots(){
+        nbShots = nbShots + 1;
+        setShots(nbShots);
+    }
+
+    function decrementShots(){
+        nbShots > 0 ? nbShots = nbShots - 1 : null;
+        setShots(nbShots);
+    }
+
+    function updateScore(){
+        score = 0;
+        
+        if(isFairway){score = score + 1;}
+        if(isRegul){score = score + 3;}
+        if(isCloser){score = score + 1;}
+
+        if(nbPutts >= 3){score = score - 1;}
+        else if(nbPutts == 1){score = score + 1;}
+        else if(nbPutts == 0){score = score + 2;}
+
+        if(nbShots >= props.nbPar+3){score = score - 1;}
+
+        setScore(score > 0 ? score : 0);
+    }
+
+    return(
+        <View style={styles.container}>
+            <Text style={styles.playerName}>{playerName}</Text>
+
+            <CheckBox style={styles.checkBox}
+                value={isFairway}
+                onValueChange={setFairway}/>
+            <CheckBox style={styles.checkBox}
+                value={isRegul}
+                onValueChange={setRegul}/>
+            <CheckBox style={styles.checkBox}
+                value={isCloser}
+                onValueChange={setCloser}/>
+
+            <View style={styles.plusMinusContainer}>
+                <TouchableOpacity style={styles.plusMinusButton} onPress={incrementPutts}>
+                    <Text style={styles.textButton}>+</Text>
+                </TouchableOpacity>
+                <Text>{nbPutts}</Text>
+                <TouchableOpacity style={styles.plusMinusButton} onPress={decrementPutts}>
+                    <Text style={styles.textButton}>-</Text>
+                </TouchableOpacity>
             </View>
-        );
-    }
+
+            <View style={styles.plusMinusContainer}>
+                <TouchableOpacity style={styles.plusMinusButton} onPress={incrementShots}>
+                    <Text style={styles.textButton}>+</Text>
+                </TouchableOpacity>
+                <Text>{nbShots}</Text>
+                <TouchableOpacity style={styles.plusMinusButton} onPress={decrementShots}>
+                    <Text style={styles.textButton}>-</Text>
+                </TouchableOpacity>
+            </View>
+
+            <Text style={styles.score}>{score}</Text>
+        </View>
+    );    
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    playerName: {
+        margin: 10
+    },
+    checkBox: {
+        width: 50,
+        height: 50,
+        margin: 10
+    },
+    plusMinusContainer: {
+        alignItems: 'center',
+        margin: 10,
+    },
+    plusMinusButton: {
+        backgroundColor: '#3a5c8e',
+        width: 50,
+        height: 20,
+        alignItems: 'center',
+        fontWeight: 'bold'
+    },
+    textButton: {
+        color: 'white',
+        textAlign: 'center'
+    },
+    score: {
+        margin: 10
+    }
+});
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,14 +1,84 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 import PlayerHole from '../components/PlayerHole.js'
 
 export default class GameScreen extends React.Component{
-
-    render(){
-      return(
-        <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-          <PlayerHole />          
-        </View>
-      );
+  constructor(){
+    super();
+    this.state = {
+      nbPlayers : 3,
+      playerList : [{name: "Nicolas"}, {name: "Bernard"}, {name: "Benjy"}]
     }
   }
+
+  render(){
+    return(
+      <View style={{flex: 1, flexDirection: 'row', marginTop: '25%'}}>
+        <View style={styles.titlesContainer}>
+          <Text style={styles.playerName}>Joueurs</Text>
+
+          <Text style={styles.boxFairway}>Fairway</Text>
+          <Text style={styles.boxRegul}>Regul</Text>
+          <Text style={styles.boxBest}>Meilleur</Text>
+
+          <Text style={styles.puttsField}>Putts</Text>
+          <Text style={styles.shotsField}>Coups</Text>
+
+          <Text style={styles.score}>Score</Text>
+        </View>
+        <FlatList
+          numColumns={Math.ceil(this.state.playerList.length)}
+          data={this.state.playerList}
+          renderItem={({item}) => <PlayerHole playerName={item.name} nbPar={3} />}
+        />
+      </View>
+    );
+  }
+
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  titlesContainer: {
+    backgroundColor: '#CFDCC3'
+  },
+  playerName: {
+      margin: 10,
+      fontWeight: 'bold'
+  },
+  boxFairway: {
+    margin: 10,
+    marginTop: 30,
+    fontWeight: 'bold'
+  },
+  boxRegul: {
+    margin: 10,
+    marginTop: 40,
+    fontWeight: 'bold'
+  },
+  boxBest: {
+    margin: 10,
+    marginTop: 40,
+    fontWeight: 'bold'
+  },
+  puttsField: {
+    margin: 10,
+    marginTop: 45,
+    fontWeight: 'bold'
+  },
+  shotsField: {
+    margin: 10,
+    marginTop: 50,
+    fontWeight: 'bold'
+  },
+  score: {
+      margin: 10,
+      marginTop: 25,
+      fontWeight: 'bold'
+  }
+});
